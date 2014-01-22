51dde6929ee22ed6d9008ffefaa40a62dae363ed
bmauser <bmauser@gmail.com>
2014-01-22 19:11:50 +0100

    change in comments

--- app/User.php
+++ app/User.php
@@ -2,13 +2,13 @@
 
 /**
  * User object.
- * 
+ *
  * It is always good to extend Psa_User and Psa_Group classes because
  * you'll probably need some custom field in the database or add some
- * method to user object.
- * See documentation for details how to extend Psa_User and Psa_Group classes:
- * http://
+ * method to the user object.
+ * See documentation for details how to extend Psa_User class:
+ * http://bojanmauser.from.hr/phpstartapp/reference/class-Psa_User.html
  */
 class User extends Psa_User {
-		
+
 }
